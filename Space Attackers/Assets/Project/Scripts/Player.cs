3e8a6545ef677c46d5ca6fc3756e6f0b20fcd1b6
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-05-02 14:56:13 +0800

    Creating missiles part 1

--- Space Attackers/Assets/Project/Scripts/Player.cs
+++ Space Attackers/Assets/Project/Scripts/Player.cs
@@ -7,6 +7,11 @@ public class Player : MonoBehaviour
 
     public float speed = 1.5f;
     public float horizontalLimit = 2.5f; // See update
+    public float firingSpeed = 3f;
+    public GameObject missilePrefab; // Game object (prefab) inserted into inspector tab for player
+
+    // Make it so that you have to release the key (left ctrl --> Fire1) to be able to shoot another missile
+    private bool fired = false; // See fire missiles in void update
 
     // Start is called before the first frame update
     void Start()
@@ -31,5 +36,19 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(-horizontalLimit, transform.position.y, transform.position.z);
             GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
         }
+
+        // Fire missiles
+        if (Input.GetAxis ("Fire1") == 1f) { // Unity editor --> Edit --> Project Settings --> Input // ^^
+            if (fired == false) {
+                GameObject missileInstance  = Instantiate (missilePrefab);
+                missileInstance.transform.SetParent(transform.parent);
+                missileInstance.transform.position = transform.position; // Set missile position (initial) --> same position as player
+                missileInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -firingSpeed); // 0 = x, firingSpeed = y pos
+                Destroy (missileInstance, 5f); // 5f = how long we want the missile to stay around for before being destroyed
+            }
+        } 
+        else {
+            fired == false;
+        } 
     }
 }
