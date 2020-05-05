4556dc45a0a31a056c4f5094f7787b23944ef708
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-05-05 20:02:10 +0800

    #1 + Enemy Creation continuation
    Added particle effects, and the ability to shoot (and destroy) the enemies + figured out how to use the apply all function for a prefab (inspector: Overrides (top right corner)). 
    
    I might create some simple backdrops in Pixilart for this project.
    
    Thanks!

--- Space Attackers/Assets/Project/Scripts/Enemy.cs
+++ Space Attackers/Assets/Project/Scripts/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    // Particle effects
+    public GameObject explosionPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,18 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // Check for collision
+    void OnTriggerEnter2D (Collider2D otherCollider) {
+        if (otherCollider.tag == "PlayerMissile") {
+            GameObject explosionInstance = Instantiate (explosionPrefab);
+            explosionInstance.transform.SetParent(transform.parent); // same as this.transfer.parent --> parent of the enemy
+            explosionInstance.transform.position = transform.position;
+
+            Destroy (explosionInstance, 1.5f); //1.5f = 1.5 seconds
+            Destroy (gameObject);
+            Destroy (otherCollider.gameObject);
+        }
     }
 }
