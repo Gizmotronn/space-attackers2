e9dc1b880144bf2e73f0fa8ab80ea427919ef9dd
Limo <liam@acordsoftware.tech>
2020-07-29 11:44:13 +0800

    #6 #1 - Building the ship
    CU-20wr45
    
    Co-Authored-By: Liam Arbuckle <liam@acordsoftware.tech>

---
+++ SpaceAttackersRedo/Assets/Project/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1.5f;
+    public float horizontalLimit = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Horizontal movement
+        GetComponent<Rigidbody2D> ().velocity = new Vector2 (
+            Input.GetAxis("Horizontal") * speed, // x
+            0 // y
+        );
+
+        // Keep the player within bounds
+        if (transform.position.x > horizontalLimit) {
+            transform.position = new Vector3(horizontalLimit, transform.position.y, transform.position.z);
+            GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+        } if (transform.position.x < -horizontalLimit) {
+            transform.position = new Vector3(-horizontalLimit, transform.position.y, transform.position.z);
+        }
+    }
+}
