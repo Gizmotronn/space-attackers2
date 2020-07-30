991b06172f47d9db50829a2eba7f2430a1c1c07e
Limo <liam@acordsoftware.tech>
2020-07-30 10:07:39 +0800

    #1 #6 - Firing Missiles
    CU-20wr45
    
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>
    Co-Authored-By: Liam Arbuckle <liam@acordsoftware.tech>

--- SpaceAttackersRedo/Assets/Project/Scripts/Player.cs
+++ SpaceAttackersRedo/Assets/Project/Scripts/Player.cs
@@ -6,6 +6,10 @@ public class Player : MonoBehaviour
 {
     public float speed = 1.5f;
     public float horizontalLimit = 2.5f;
+    public float firingSpeed = 3f;
+    public GameObject missilePrefab;
+
+    private bool fired = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +32,22 @@ public class Player : MonoBehaviour
             GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
         } if (transform.position.x < -horizontalLimit) {
             transform.position = new Vector3(-horizontalLimit, transform.position.y, transform.position.z);
+        } 
+
+        // Fire missile
+        if (Input.GetAxis ("Fire1") == 1f) {
+            if (fired == false) {
+                fired = true;
+                //Debug.Log ("Fire!");
+
+                GameObject missileInstance = Instantiate (missilePrefab);
+                missileInstance.transform.SetParent (transform.parent);
+                missileInstance.transform.position = transform.position;
+                missileInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(0, firingSpeed);
+                Destroy(missileInstance, 2f);
+            }            
+        } else {
+            fired = false;
         }
     }
 }
